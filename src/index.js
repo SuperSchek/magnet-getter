aec2d633c4722cf47657ca430c9c88b3f34bcfd5
Sander Schekman <sanderschekman@gmail.com>
2020-03-05 12:52:10 +0100

    feat(crawler): adds method to get magnet link from detail page

--- src/index.js
+++ src/index.js
@@ -7,19 +7,26 @@ const agent = {
     CATEGORY_PLACEHOLDER: "%category%"
   },
   SELECTORS: {
-    RESULTS: ".table-list tbody tr",
-    RESULT_NAME: ".name",
-    DETAIL_PAGE_URL: ".name a:nth-child(2)",
-    SEEDS: ".seeds",
-    LEECHES: ".leeches",
-    SIZE: ".size",
-    DATE: ".coll-date"
+    QUERY: {
+      RESULTS: ".table-list tbody tr",
+      RESULT_NAME: ".name",
+      DETAIL_PAGE_URL: ".name a:nth-child(2)",
+      SEEDS: ".seeds",
+      LEECHES: ".leeches",
+      SIZE: ".size",
+      DATE: ".coll-date"
+    },
+    DETAIL: {
+      MAGNET_LINK: ".torrent-detail-page a"
+    }
   },
   categories: {
     movies: "Movies",
     tv: "TV"
   }
 };
+
+const queryResults = [];
 
 async function getPropertyValue(element, selector, property = "innerText") {
   try {
@@ -42,39 +49,64 @@ async function makeTheQuery() {
   const page = await browser.newPage();
   await page.goto(url);
 
-  const results = await page.$$(agent.SELECTORS.RESULTS);
+  const results = await page.$$(agent.SELECTORS.QUERY.RESULTS);
   const response = [];
 
   for (let i = 0; i < results.length; i++) {
     const result = results[i];
-    const name = await getPropertyValue(result, agent.SELECTORS.RESULT_NAME);
+    const name = await getPropertyValue(
+      result,
+      agent.SELECTORS.QUERY.RESULT_NAME
+    );
     const url = await getPropertyValue(
       result,
-      agent.SELECTORS.DETAIL_PAGE_URL,
+      agent.SELECTORS.QUERY.DETAIL_PAGE_URL,
       "href"
     );
-    const seeds = await getPropertyValue(result, agent.SELECTORS.SEEDS);
-    const leeches = await getPropertyValue(result, agent.SELECTORS.LEECHES);
-    const size = await getPropertyValue(result, agent.SELECTORS.SIZE);
-    const date = await getPropertyValue(result, agent.SELECTORS.DATE);
+    const seeds = await getPropertyValue(result, agent.SELECTORS.QUERY.SEEDS);
+    const leeches = await getPropertyValue(
+      result,
+      agent.SELECTORS.QUERY.LEECHES
+    );
+    const size = await getPropertyValue(result, agent.SELECTORS.QUERY.SIZE);
+    const date = await getPropertyValue(result, agent.SELECTORS.QUERY.DATE);
     // const poster = await PuppeteerUtility.getPropertyValue(
     //   result,
-    //   constants.RESULTS_QUERY_SELECTORS.RESULT_POSTER_URL,
+    //   constants.RESULTS_QUERY_SELECTORS.QUERY.RESULT_POSTER_URL,
     //   "src"
     // );
     // const year = await PuppeteerUtility.getPropertyValue(
     //   result,
-    //   constants.RESULTS_QUERY_SELECTORS.RESULT_YEAR
+    //   constants.RESULTS_QUERY_SELECTORS.QUERY.RESULT_YEAR
     // );
 
     console.log({ name, url, seeds, leeches, size, date });
 
-    response.push({
-      name
-    });
+    queryResults.push({ name, url, seeds, leeches, size, date });
   }
 
   await browser.close();
+
+  getMagnetLink(1);
 }
+
+const getMagnetLink = async index => {
+  const magnet = queryResults[index];
+
+  console.log("getting magnet link for " + magnet.name);
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(magnet.url);
+
+  const magnetLink = await (
+    await (await page.$(agent.SELECTORS.DETAIL.MAGNET_LINK)).getProperty("href")
+  ).jsonValue();
+  console.log(magnetLink);
+
+  return magnetLink;
+
+  // const magnetLink = await page.$$(agent.SELECTORS.DETAIL.RESULTS);
+};
 
 makeTheQuery();
